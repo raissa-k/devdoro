afcb8d3ff11f81f6dba4702bdae2b2305abb47cf
Raissa K <raissakuzerdev@gmail.com>
2022-11-04 18:30:38 -0300

    Cleaned initial commit files
    Migrated from previous React version:
    - ExperienceBar component
    - Styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { ExperienceBar } from '../components/ExperienceBar'
+import styles from '../styles/pages/Home.module.css'
+
+export default function Home() {
+	return (
+    	<div className={styles.container}>
+			<ExperienceBar />
+		</div>
+  )
+}
