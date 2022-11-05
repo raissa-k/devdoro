4aa6e1bce9d6c72a6b7c8eea8f519ccfd7a90100
Raissa K <raissakuzerdev@gmail.com>
2022-11-05 16:26:20 -0300

    Adds countdown reset
    - Countdown button and button text changes depending on countdown state
    - Uses NodeJS.Timeout to stop setTimeout on button click
    
    Extra: placeholder Pokemon profile picture and name

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,9 +3,9 @@ import styles from '../styles/components/Profile.module.css'
 export function Profile(){
 	return (
 		<div className={styles.profileContainer}>
-			<img src="https://github.com/raissa-k.png" alt="Profile picture"/>
+			<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/92.svg" alt="Profile picture"/>
 			<div>
-				<strong>Raissa K.</strong>
+				<strong>Gastly</strong>
 				<p>
 					<img src="icons/level.svg" alt="Level up symbol" />
 					Level 1
