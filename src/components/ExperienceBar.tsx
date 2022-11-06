828822e84f9129d9710860199e61fac8aab7fb17
Raissa K <raissakuzerdev@gmail.com>
2022-11-06 14:28:27 -0300

    Added context to components
    - Experience bar updates with progress
    - Experience necessary to level updates with progress
    - Challenge box text reflects the randomly picked challenge
    
    Extra:
    - Updates body/eye svg icons and their colors

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,14 +1,20 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengeContext'
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
+	const {currentExperience,experienceToNextLevel} = useContext(ChallengesContext)
+
+	const percentToNextLevel = Math.round(currentExperience * 100)/experienceToNextLevel
+
 	return (
 		<header className={styles.experienceBar}>
 			<span>0 exp</span>
 			<div>
-				<div style={{ width: '50%' }}/>
-				<span style={{ left: '50%' }} className={ styles.currentExperience }>300 exp</span>
+				<div style={{ width: `${percentToNextLevel}` }}/>
+				<span style={{ left: `${percentToNextLevel}` }} className={ styles.currentExperience }>{currentExperience}</span>
 			</div>
-			<span>600 exp</span>
+			<span>{experienceToNextLevel}</span>
 		</header>
 	)
 }
