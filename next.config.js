f64cd4bbbd12bf59332178f279bb2a3baa16ae90
Raissa K <raissakuzerdev@gmail.com>
2022-11-06 23:58:59 -0300

    Adds PWA functionality through next-pwa
    - Includes new icons in _app

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
-}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+	dest: 'public'
+  })
+
+module.exports = withPWA({
+	reactStrictMode: true,
+	swcMinify: true,
+	i18n: {
+	locales: ['en'],
+	defaultLocale: 'en',
+	},
+  })
