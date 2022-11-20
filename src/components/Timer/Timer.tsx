7c8e243d8ba8b1681f67659ea3b623187eca3693
Raissa K <raissakuzerdev@gmail.com>
2022-11-19 23:44:54 -0300

    Adds break timers
    Closes #25
    
    - Moves Timer's useState + useContext hook logic to new useReduce files
    - In addition to Pomodoro (25 mins),
      adds long (20 mins) and short (5 mins) break mode options
    - Splits Timer into Clock, ClockActionsMenu and ClockModeMenu
    - Displays waves on Clock's background that rise as cycle progresses
    - Upon cycle completion:
      - Displays pulse animation on Exercise button
      - Adds "end" and "replay" options to Exercise modal
      - "Skip" button remains actionable
      - Plays sound

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,20 +1,22 @@
-import { CountdownProvider } from "../../contexts/CountdownContext";
-import { Countdown } from "./Countdown";
 import { Exercise } from "../Exercise";
 import ModalForm from "../Menus/Modal";
 import ModalProvider from "../../contexts/ModalContext";
+import TimerProvider from "../../contexts/TimerContext";
+import Clock from "./Clock";
 
 export function Timer(){
 	return (
-		<CountdownProvider>
+		<>
+		<TimerProvider>
 		<ModalProvider>
 		<section aria-label="Timer" role={"tabpanel"} className="flex flex-col items-center min-h-screen">
-			<Countdown />
+			<Clock />
 			<ModalForm modalTitle="Move around!">
 				<Exercise />
 			</ModalForm>
 		</section>
 		</ModalProvider>
-		</CountdownProvider>
+		</TimerProvider>
+		</>
 	)
 }
